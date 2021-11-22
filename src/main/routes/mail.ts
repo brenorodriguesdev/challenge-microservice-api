cc45efb7dff056c23121115cb242b4a7e1d719ee
“Breno <brenorodriguesdev@gmail.com>
2021-11-21 21:58:34 -0300

    feat: add auth key middleware on send mail route

--- src/main/routes/mail.ts
+++ src/main/routes/mail.ts
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import { adaptRouter } from "../adapters/express-controller"
+import { adaptMiddleware } from "../adapters/express-middleware"
 import { makeSendEmailController } from "../factories/controllers/send-email"
+import { makeAuthKeyMiddleware } from "../factories/middlewares/auth-key"
 
 export default (router: Router): void => {
-    router.post('/send', adaptRouter(makeSendEmailController()))
+    router.post('/send', adaptMiddleware(makeAuthKeyMiddleware()),adaptRouter(makeSendEmailController()))
 }
