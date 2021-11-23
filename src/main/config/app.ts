56b8acd75ea4825dbbfa69633d35c7bd1a8623e4
“Breno <brenorodriguesdev@gmail.com>
2021-11-23 02:08:11 -0300

    feat: setup origin cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -5,8 +5,19 @@ import setupSwagger from './swagger'
 
 const app = express()
 
+var allowedOrigins = ['http://localhost:4500'];
+
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'Você não tem permissão para acessar este serviço!';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}))
 setupSwagger(app)
 setupRoutes(app)
 
