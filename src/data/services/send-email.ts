30f5c8fa2c86108ecd83020772e3a79e9ea01d69
“Breno <brenorodriguesdev@gmail.com>
2021-11-23 01:51:56 -0300

    fix: send email service

--- src/data/services/send-email.ts
+++ src/data/services/send-email.ts
@@ -1,20 +1,26 @@
+import e from "express"
 import { SendEmailRequestModel } from "../../domain/models/send-email-request"
 import { SendEmailResponseModel } from "../../domain/models/send-email-response"
 import { SendEmailUseCase } from "../../domain/useCases/send-email"
 import { MailProvider } from "../contracts/providers/mail-provider"
 
 export class SendEmailService implements SendEmailUseCase {
-    constructor (private readonly mailProvider: MailProvider) { }
+    constructor(private readonly mailProvider: MailProvider) { }
     async send(request: SendEmailRequestModel): Promise<SendEmailResponseModel[]> {
-        let to = ""
-        request.emails.map(email => {
-            to = to + " " + email + ","
-        })
-        await this.mailProvider.send({
-           to,
-           subject: request.subjectEmail,
-           text: request.htmlEmail
-        })
-        return []
+        let sendEmailResponseModels = []
+        await Promise.all(request.emails.map(async email => {
+            const mailResponse = await this.mailProvider.send({
+                to: email,
+                subject: request.subjectEmail,
+                text: request.htmlEmail
+            })
+            sendEmailResponseModels.push({
+                email,
+                sent: mailResponse.sent,
+                error: mailResponse.error
+            })
+        }))
+
+        return sendEmailResponseModels
     }
 }
