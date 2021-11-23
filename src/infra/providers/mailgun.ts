07a8a1d48a35aae1bfc1928020ee17e2290d234d
“Breno <brenorodriguesdev@gmail.com>
2021-11-23 01:41:57 -0300

    fix: mailprovider implements

--- src/infra/providers/mailgun.ts
+++ src/infra/providers/mailgun.ts
@@ -1,19 +1,17 @@
 import { MailProvider, MailRequest, MailResponse } from "../../data/contracts/providers/mail-provider"
-import * as mailgun from 'mailgun-js'
+import Mailgun from 'mailgun-js'
 
 export class MailgunProvider implements MailProvider {
     async send(request: MailRequest): Promise<MailResponse> {
-        const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN });
-        const data = {
-            from: `${process.env.MAILGUN_NAME} <${process.env.MAILGUN_DOMAIN}>`,
-            to: request.to,
-            subject: request.subject,
-            text: request.text
-        };
         try {
-            mg.messages().send(data, function (error, body) {
-                console.log(body);
-            });
+            const mg = new Mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN });
+            const data = {
+                from: `${process.env.MAILGUN_NAME} <${process.env.MAILGUN_DOMAIN}>`,
+                to: request.to,
+                subject: request.subject,
+                text: request.text
+            };
+            await mg.messages().send(data);
             return {
                 sent: true
             }
